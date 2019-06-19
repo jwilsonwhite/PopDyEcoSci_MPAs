916eb30a0cde464c12a907c6690728d7074925c9
Will White <will.white@oregonstate.edu>
2019-06-19 14:24:25 -0700

    persistence code, fix bugs in coastline

---
+++ persistence.R
@@ -0,0 +1,19 @@
+# Determine whether a coastline with MPAs is persistent or not
+# Inputs:
+# X = landscape
+# D = dispersal matrix
+# CRT = minimum value of FLEP for replacement
+# FLEP.out = value of FLEP in fished patches
+
+persistence <- function(X,D,CRT,FLEP.out){
+  
+  FLEP.vec = X
+  FLEP.vec[X==0] = FLEP.out # put in correct value of FLEP in fished areas
+  FLEP.mat = t(matrix(FLEP.vec,nrow=length(X),ncol=length(X))) # make this a matrix of production in each patch
+  
+  C = D * FLEP.mat / CRT # full projection matrix. dispersal x production / minimum replacement rate
+  
+  E = max(abs(eigen(C)$values))
+  
+  return(E>1)
+} # end function
